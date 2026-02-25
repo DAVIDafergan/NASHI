1540b2198b7490adc713db3b6e18686e74e2218b
DAVIDafergan <DAVIDAFERGAN999@gmail.com>
2026-02-26 00:17:26 +0200

    Final fix: Merged theme variables with Tailwind directives

--- index.tsx
+++ index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import './theme.css'; // <--- השורה הזו היא המפתח לעיצוב!
 import App from './App';
 
 const rootElement = document.getElementById('root');
