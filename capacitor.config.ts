e9b4020c3129802730d84f08b6e544be3a795717
DAVIDafergan <DAVIDAFERGAN999@gmail.com>
2026-03-11 02:35:34 +0200

    Add Capacitor config and Android platform

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.nashi.app',
+  appName: 'nashi',
+  webDir: 'dist'
+};
+
+export default config;
